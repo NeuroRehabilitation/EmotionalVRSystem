f9dde9d38f4cf5284e1220787b76af95ea812480
rodrigolima6 <rodrigoolivallima6@gmail.com>
2023-07-11 16:16:16 +0100

    feat(SAM): Create SAM scale and finish VAS scale.

--- Assets/VAS.cs
+++ Assets/VAS.cs
@@ -1,25 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class VAS : MonoBehaviour
 {
-    [Header("VAS Scales")]
-    public ToggleGroup AngerToggles;
-    public ToggleGroup FearToggles;
-    public ToggleGroup JoyToggles;
-    public ToggleGroup SadToggles;
+    [Header("VAS Toggle Scales")]
+    public ToggleGroup[] toggles;
 
-    private string AngerRating;
-    private string FearRating;
-    private string JoyRating;
-    private string SadRating;
+    [Header("VAS Gameobjects")]
+    public GameObject[] VAS_Items;
+
+    [Header("Next Button")]
+    public Button NextButton;
+
+    public string[] answers = new string[4];
+
+    private int currentToggle = 0;
+
+    Toggle selected = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            toggles[i].allowSwitchOff = true;
+
+            foreach(Toggle toggle in toggles[i].GetComponentsInChildren<Toggle>()) 
+            {
+                toggle.onValueChanged.AddListener(OnToggleChanged);
+            }
+        }
+
+        NextButton.interactable = false;
+    }
+
+    private void Update()
+    {
+        //GetSelectedToggle();
+    }
+
+    void OnToggleChanged(bool isOn)
+    {
+        if (isOn) 
+        {
+            selected = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.GetComponent<Toggle>();
+            NextButton.interactable = true;
+        }
+        else
+            NextButton.interactable = false;
+           
+        Debug.Log(selected);
+    }
+
+    public void Submit()
+    {
+        //if (currentToggle >= 3)
+
+        answers[currentToggle] = selected.name;
+        VAS_Items[currentToggle].SetActive(false);
+        currentToggle++;
+        VAS_Items[currentToggle].SetActive(true);
 
     }
 }
